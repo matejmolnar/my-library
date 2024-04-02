b61327a87b8f15deebceed0d645500fccabd7ab3
Matej Molnar <matej.molnar@outlook.com>
2024-04-02 17:59:54 +0200

    [feat] add docs

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,5 +1,25 @@
 import Foundation
-/** MyLibrary is awesome
- */
+/// # MyLibrary
+/// `MyLibrary` is a simple demonstration library.
+///
+/// ## Overview
+/// This library provides a basic example of how to create a Swift library with documentation using DocC.
+///
+/// ## Usage
+/// Here's how you can use `MyLibrary`:
+///
+/// ```swift
+/// let myLibrary = MyLibrary()
+/// myLibrary.sayHello()
+/// ```
 public class MyLibrary {
+    /// Initializes a new instance of `MyLibrary`.
+    public init() {}
+
+    /// Prints "Hello, world!" to the console.
+    ///
+    /// This function simply prints a greeting message to the console.
+    public func sayHello() {
+        print("Hello, world!")
+    }
 }
